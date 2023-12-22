f04b5263e65e7bd4f1cb0ac60d26eb42b1547886
Luis Mata <mata.luism@gmail.com>
2023-12-22 10:35:28 +0100

    Bootstrap application. Added first AccountServiceTest draft

---
+++ src/main/java/com/luismata/demolayeredarch/DemolayeredarchApplication.java
@@ -0,0 +1,13 @@
+package com.luismata.demolayeredarch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemolayeredarchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemolayeredarchApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/luismata/demolayeredarch/services/AccountServiceTests.java
@@ -0,0 +1,34 @@
+package com.luismata.demolayeredarch.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+@SpringBootTest
+public class AccountServiceTests {
+
+    @Autowired
+    private AccountService accountService;
+
+    @BeforeEach
+    void init() {
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = {"1", "2"})
+    void givenValidCustomer_whenCallingCreateAccount_thenNewAccountForCustomerCreated(int accountOwnerCustomerId) {
+        // given
+
+        //when
+        Account newAccount = accountService.createNewAccount(accountOwnerCustomerId);
+
+        //then
+        assertNotNull(newAccount);
+        assertEquals(newAccount.getAccountOwnerCustomerId(), accountOwnerCustomerId);
+    }
+}
